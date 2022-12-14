d3d3bf3be7e70a2e0c7dc9684d01efa8a569cb21
noxtua-code <[200~93567294+noxtua-code@users.noreply.github.com>
2022-12-14 17:06:56 +1000

    Completed first 7 exercises, as part of the Odin Project Foundations course

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...elements) {
+    let newArray = [];
 
+    for (let element of elements) {
+        for (let i = 0; i < originalArray.length; i++)
+        if (originalArray[i] === element) {
+            originalArray.splice(i, 1);
+            i--;
+        }
+    }
+    return originalArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR'
+    } else if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    } 
+    
+    
+    if (num1 === num2) {
+        return num1;
+    } else {
+        return num1 + sumAll(num1 + 1, num2);
+    }
+    
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
